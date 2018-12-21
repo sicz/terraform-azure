c72ad86b7899b66e892919bb02f04e20e10ce6a9
Petr Řehoř <rx@rx.cz>
2018-12-21 16:10:08 +0100

    Rename targets in terraform.mk

--- terraform.mk
+++ terraform.mk
@@ -1,31 +1,29 @@
 TF_DEPENDENCIES	+= $(shell ls *.tf *.tf.json *.tfvars 2>/dev/null)
 
-init .terraform: $(TF_DEPENDENCIES)
+tf-init .terraform: $(TF_DEPENDENCIES)
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
 	@terraform init $(TF_INIT_OPTS)
 
-plan: .terraform
+tf-plan: .terraform
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
 	@terraform plan $(TF_PLAN_OPTS)
 
-apply terraform.tfstate: .terraform
+tf-apply terraform.tfstate: .terraform
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
 	@terraform apply $(TF_APLLY_OPTS)
 
-output: terraform.tfstate
+tf-output: terraform.tfstate
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
 	@terraform output $(TF_OUTPUT_OPTS)
 
-destroy:
+tf-destroy:
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
 	@[ ! -f terraform.tfstate ] || terraform destroy $(TF_DESTROY_OPTS)
 	@rm -f terraform.tfstate
 
-clean:
-	rm -fv terraform.tfstate
+tf-forget:
+	[ -f terraform.tfstate ] && mv -fv terraform.tfstate terraform.tfstate.backup
 
-distclean:
+tf-clean:
 	rm -fv *.tfstate *.tfstate.*
 	rm -frv .terraform
-
-clobber: destroy distclean
