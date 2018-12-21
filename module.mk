5c93133cdfc7b3086787bf21e70e7b0e74e8b237
Petr Řehoř <rx@rx.cz>
2018-12-21 12:27:34 +0100

    Fix module.mk

--- module.mk
+++ module.mk
@@ -1,22 +1,24 @@
-TF_DEPENDECIES	+= *.tf *.tf.json *.tfvars
+TF_DEPENDENCIES	+= $(shell ls *.tf *.tf.json *.tfvars 2>/dev/null)
 
-get-access-token:
-	@az account get-access-token ${AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS}> /dev/null
-
-init .terraform: get-access-token $(TF_DEPENDECIES)
-	@terraform init ${TF_INIT_OPTS}
+init .terraform: $(TF_DEPENDENCIES)
+	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
+	@terraform init $(TF_INIT_OPTS)
 
 plan: .terraform
-	@terraform plan ${TF_PLAN_OPTS}
+	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
+	@terraform plan $(TF_PLAN_OPTS)
 
 apply terraform.tfstate: .terraform
-	@terraform apply ${TF_APLLY_OPTS}
+	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
+	@terraform apply $(TF_APLLY_OPTS)
 
 output: terraform.tfstate
-	@terraform output ${TF_OUTPUT_OPTS}
+	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
+	@terraform output $(TF_OUTPUT_OPTS)
 
-destroy: get-access-token
-	@[ ! -f terraform.tfstate ] || terraform destroy ${TF_DESTROY_OPTS}
+destroy:
+	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
+	@[ ! -f terraform.tfstate ] || terraform destroy $(TF_DESTROY_OPTS)
 	@rm -f terraform.tfstate
 
 clean:
