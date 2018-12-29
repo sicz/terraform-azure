f4bff00ff2ce0885c2ac7ed1488d94cce9e7b1d2
Petr Řehoř <rx@rx.cz>
2018-12-29 21:50:25 +0100

    Fix make destroy with 0.12 HCL syntax

--- terraform.mk
+++ terraform.mk
@@ -20,7 +20,7 @@ tf-output: terraform.tfstate
 
 tf-destroy:
 	@az account get-access-token $(AZ_ACCOUNT_GET_ACCESS_TOKEN_OPTS) >/dev/null
-	@[ ! -f terraform.tfstate ] || terraform destroy $(TF_DESTROY_OPTS)
+	@[ ! -f terraform.tfstate ] || $(TF_COMMAND) destroy $(TF_DESTROY_OPTS)
 	@rm -f terraform.tfstate
 
 tf-forget:
